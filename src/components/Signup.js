ecd51ff772f8b123f508d0a9cc7e193fb1fa16b2
이은형 <leh237@gmail.com>
2022-07-25 17:52:09 +0900

    회원가입 유효성검사

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,65 +1,124 @@
-import React, {useState}from "react";
+import React, { useRef, useState } from "react";
 import styled from "styled-components";
 import { useNavigate, Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
-import { actionCreators as userActions } from "../redux/modules/user";
-import { idCheck, usernameCheck } from "../shared/common";
-
 const Signup = () => {
   const navigate = useNavigate();
+  // 포커스를 주기 위한 useRef
+  const inutRef = useRef([]); // ref 배열형태로 저장해서 여러 값을 인덱스로 컨트롤 가능
 
-  const dispatch = useDispatch();
+  // input value state 관리
+  const [inputs, setInputs] = useState({
+    id: "",
+    password: "",
+    email: "",
+  });
+  const { id, password, email } = inputs; // 구조분해할당
 
-  const [userId, setUserId] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [pwd_check, setPwdCheck] = useState("");
-  const [gender, setGender] = useState("");
+  //유효한 id, password, email 조건 변수에 담아 사용
+  const vaildId = id.length >= 3 && id.length <= 10;
+  const vaildPassword = password.length >= 12 && password.length <= 20;
+  const regexp = /^[0-9a-zA-Z]+@[0-9a-zA-Z]+\.[0-9a-zA-Z]/; // email 형식 정규표현식
+  const vaildEmail = email.match(regexp);
 
-  const signup = () => {
-    if (
-      userId === "" ||
-      password === "" ||
-      username === "" ||
-      pwd_check === "" ||
-      gender === ""
-    ) {
-      window.alert("모두 입력해주세요!");
-      return;
-    }
-
-    if (!idCheck(userId)) {
-      window.alert("아이디 형식이 맞지 않습니다!");
-      return;
-    }
-
-    if (!usernameCheck(username)) {
-      window.alert("닉네임에는 기호가 없어야 합니다!");
-      return;
-    }
-
-    if (password !== pwd_check) {
-      window.alert("패스워드와 패스워드 확인이 일치하지 않습니다!");
-      return;
-    }
-
-    dispatch(userActions.signUpDB(userId, password, username, gender));
+  // onChange 함수로 state 값 바꿔주기
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
   };
-  
+
+  // 클릭이벤트 : 유효성에 맞는 이벤트 이루어지도록
+  const handleClick = () => {
+    if (!vaildId) {
+      alert("유효하지 않은 nickname 입니다."); // 알람창
+      setInputs({
+        // 값 비워주기
+        ...inputs,
+        id: "", // 바뀐 값 빼고 나머지는 그대로 스프레드 연산자
+      });
+
+      inutRef.current[0].focus(); // 자동 포커스
+    } else if (!vaildPassword) {
+      alert("유효하지 않은 password 입니다.");
+      inutRef.current[1].focus();
+      setInputs({
+        ...inputs,
+        password: "",
+      });
+
+    } else if (!vaildEmail) {
+      alert("유효하지 않은 email 입니다.");
+      inutRef.current[2].focus();
+      setInputs({
+        ...inputs,
+        email: "",
+      });
+
+    } else {
+      return alert("회원가입 완료!");
+    }
+  };
+
   return (
-    <Container>
-      <H2>회원정보</H2>
-      <Input label="이메일" placeholder="이메일" />
-      <Input label="닉네임" placeholder="닉네임" />
-      <Input label="주소" placeholder="__시" />
-      <Input label="비밀번호" type="password" placeholder="비밀번호" />
-      <Input label="비밀번호 확인" type="password" placeholder="비밀번호 확인" />
-      <Button onClick={() => navigate("/")}>가입하기</Button>
-      <P>
-        회원이신가요?<Link to="/login">로그인</Link>
-      </P>
-    </Container>
+    <>
+      <Container>
+        <H2>회원가입</H2>
+        <Input
+          type="text"
+          name="email"
+          placeholder="이메일"
+          value={email}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[2] = el)}
+        />
+
+        <Input
+          type="text"
+          name="id"
+          placeholder="닉네임"
+          value={id}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[0] = el)}
+        />
+        <Input
+          type="text"
+          name="password"
+          placeholder="주소 OO시"
+          value={password}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[1] = el)}
+        />
+
+        <Input
+          type="text"
+          name="password"
+          placeholder="비밀번호"
+          value={password}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[1] = el)}
+        />
+
+        <Input
+          type="text"
+          name="password"
+          placeholder="비밀번호 확인"
+          value={password}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[1] = el)}
+        />
+
+        <Button
+          type="button"
+          onClick={handleClick}
+          disabled={id.length < 1 && password.length < 1 && email.length < 1}
+        >
+          회원가입
+        </Button>
+      </Container>
+    </>
   );
 };
 
@@ -87,7 +146,6 @@ const P = styled.div`
   align-items: center;
   justify-content: center;
   margin-top: 50px;
-  
 `;
 
 const Input = styled.input`
