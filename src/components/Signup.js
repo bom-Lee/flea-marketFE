0d88850e1d0fe1901fef69b229883aebe9aadd32
이은형 <leh237@gmail.com>
2022-07-26 15:27:46 +0900

    회원페이지

---
+++ src/components/Signup.js
@@ -0,0 +1,217 @@
+import React, { useRef, useState } from "react";
+import styled from "styled-components";
+import { useNavigate, Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+
+const Signup = () => {
+  const navigate = useNavigate();
+  // 포커스를 주기 위한 useRef
+  const inutRef = useRef([]); // ref 배열형태로 저장해서 여러 값을 인덱스로 컨트롤 가능
+
+  // input value state 관리
+  const [inputs, setInputs] = useState({
+    email: "",
+    id: "",
+    adress: "",
+    pw: "",
+    pwcheck: "",
+  });
+  const { email, id, adress, pw, pwcheck } = inputs; // 구조분해할당
+
+  //유효한 id, password, email 조건 변수에 담아 사용
+  const regexp = /^[0-9a-zA-Z]+@[0-9a-zA-Z]+\.[0-9a-zA-Z]/; // email 형식 정규표현식
+  const vaildEmail = email.match(regexp);
+  const vaildId = id.length >= 3 && id.length <= 10;
+  const vaildAdress =
+    inputs.adress.length >= 2 &&
+    inputs.adress[inputs.adress.length - 1] === "시";
+  const vaildPw = pw.length >= 8 && pw.length <= 20;
+  const vaildPwcheck = pwcheck.length >= 8 && pwcheck.length <= 20;
+
+  // onChange 함수로 state 값 바꿔주기
+  const handleChange = (e) => {
+    setInputs({
+      ...inputs,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // 클릭이벤트 : 유효성에 맞는 이벤트 이루어지도록
+  const handleClick = (e) => {
+    const passwordDoubleCheck = (pw, pwcheck) => {
+      if (pw !== pwcheck) {
+        alert("비밀번호가 다릅니다.");
+      } else {
+        alert("비밀번호가 동일합니다");
+      }
+    };
+
+    passwordDoubleCheck(inutRef.current[3].value, inutRef.current[4].value);
+
+    if (!vaildEmail) {
+      e.preventDefault(); // 유효성 검사를 통화했을 경우 link통해 컴포넌트 간 동동
+      alert("유효하지 않은 email 입니다.");
+      inutRef.current[0].focus(); // 자동 포커스
+      setInputs({
+        // 값 비워주기
+        ...inputs,
+        email: "", // 바뀐 값 빼고 나머지는 그대로 스프레드 연산자
+      });
+    } else if (!vaildId) {
+      e.preventDefault();
+      alert("유효하지 않은 nickname 입니다.");
+      setInputs({
+        ...inputs,
+        id: "",
+      });
+      inutRef.current[1].focus();
+    } else if (!vaildAdress) {
+      e.preventDefault();
+      alert("유효하지 않은 주소 입니다.");
+      inutRef.current[2].focus();
+      setInputs({
+        ...inputs,
+        adress: "",
+      });
+    } else if (!vaildPw) {
+      e.preventDefault();
+      alert("유효하지 않은 password 입니다.");
+      inutRef.current[3].focus();
+      setInputs({
+        ...inputs,
+        password: "",
+      });
+    } else if (!vaildPwcheck) {
+      e.preventDefault();
+      alert("유효하지 않은 password 입니다.");
+      inutRef.current[4].focus();
+      setInputs({
+        ...inputs,
+        password: "",
+      });
+    } else {
+      //api요청만들기
+      return alert("회원가입 완료!");
+    }
+  };
+
+  return (
+    <>
+      <Container>
+        <H2>회원가입</H2>
+        <Input
+          type="text"
+          name="email"
+          placeholder="이메일"
+          value={email}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[0] = el)}
+        />
+
+        <Input
+          type="text"
+          name="id"
+          placeholder="닉네임"
+          value={id}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[1] = el)}
+        />
+        <Input
+          type="text"
+          name="adress"
+          placeholder="주소 OO시"
+          value={adress}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[2] = el)}
+        />
+
+        <Input
+          type="text"
+          name="pw"
+          placeholder="비밀번호"
+          value={pw}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[3] = el)}
+        />
+
+        <Input
+          type="text"
+          name="pwcheck"
+          placeholder="비밀번호 확인"
+          value={pwcheck}
+          onChange={handleChange}
+          ref={(el) => (inutRef.current[4] = el)}
+        />
+
+        <Button
+          type="button"
+          onClick={handleClick}
+          disabled={id.length < 1 && pw.length < 1 && email.length < 1}
+        >
+          회원가입
+        </Button>
+        <P>
+          회원이신가요? <Link to="/login">로그인</Link>
+        </P>
+      </Container>
+    </>
+  );
+};
+
+
+const Container = styled.div`
+  width: 400px;
+  height: 360px;
+  border: solid 1px #dadada;
+  display: inline-block;
+  margin-top: 100px;
+  padding: 30px;
+`;
+
+const H2 = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 25px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const P = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  margin-top: 50px;
+`;
+
+const Input = styled.input`
+  position: relative;
+  overflow: hidden;
+  width: 100%;
+  height: 40px;
+  margin: 0 0 8px;
+  padding: 5px 39px 5px 15px;
+  border: solid 1px #dadada;
+  border-radius: 8px;
+  background: #fff;
+  box-sizing: border-box;
+`;
+
+const Button = styled.div`
+  font-size: 18px;
+  font-weight: 700;
+  line-height: 49px;
+  display: block;
+  width: 100%;
+  height: 49px;
+  margin: 16px 0 7px;
+  cursor: pointer;
+  text-align: center;
+  color: slateblue;
+  border: none;
+  border-radius: 8px;
+  background-color: black;
+`;
+
+export default Signup;
